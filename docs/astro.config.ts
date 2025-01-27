ee1bd45c934b29739ad82ed919e1481bb6bed222
Benjamin Becquet <benjamin.becquet@pasqal.com>
2025-01-27 15:27:35 +0100

    Add an example page

--- docs/astro.config.ts
+++ docs/astro.config.ts
@@ -1,24 +1,25 @@
-import starlight from '@astrojs/starlight'
-import { defineConfig } from 'astro/config'
-import starlightClientMermaid from 'starlight-client-mermaid'
+import starlight from "@astrojs/starlight";
+import { defineConfig } from "astro/config";
+import starlightClientMermaid, {
+  remarkTagMermaid,
+} from "starlight-client-mermaid";
 
 export default defineConfig({
   integrations: [
     starlight({
       editLink: {
-        baseUrl: 'https://github.com/bbecquet/starlight-client-mermaid/edit/main/docs/',
+        baseUrl:
+          "https://github.com/bbecquet/starlight-client-mermaid/edit/main/docs/",
       },
       plugins: [starlightClientMermaid()],
-      sidebar: [
-        {
-          label: 'Start Here',
-          items: [{ slug: 'getting-started' }],
-        },
-      ],
+      sidebar: ["getting-started", "examples"],
       social: {
-        github: 'https://github.com/bbecquet/starlight-client-mermaid',
+        github: "https://github.com/bbecquet/starlight-client-mermaid",
       },
-      title: 'starlight-client-mermaid',
+      title: "starlight-client-mermaid",
     }),
   ],
-})
+  markdown: {
+    remarkPlugins: [remarkTagMermaid],
+  },
+});
